fa1fb970f390f534505011b6489b6d41e5a9c4d3
WizyHuge <hom.semen@yandex.ru>
2026-05-17 00:00:17 +0500

    PgUp/PgDown зум карты

--- solution.py
+++ solution.py
@@ -4,32 +4,46 @@ import requests
 from PIL import Image
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel
+from PyQt6.QtCore import Qt
 
 map_key = "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13"
 
-lon = "37.6176"
-lat = "55.7558"
-z = "10"
 
-resp = requests.get("https://static-maps.yandex.ru/v1", params={
-    "ll": f"{lon},{lat}",
-    "z": z,
-    "apikey": map_key
-})
+class MapWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.lon = 37.6176
+        self.lat = 55.7558
+        self.z = 10
+        self.setGeometry(100, 100, 600, 450)
+        self.setWindowTitle("Карта")
+        self.label = QLabel(self)
+        self.label.move(0, 0)
+        self.label.resize(600, 450)
+        self.load_map()
 
-with open("map.png", "wb") as f:
-    f.write(resp.content)
+    def load_map(self):
+        resp = requests.get("https://static-maps.yandex.ru/v1", params={
+            "ll": f"{self.lon},{self.lat}",
+            "z": self.z,
+            "apikey": map_key
+        })
+        with open("map.png", "wb") as f:
+            f.write(resp.content)
+        self.label.setPixmap(QPixmap("map.png"))
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key.Key_PageUp:
+            if self.z < 17:
+                self.z += 1
+                self.load_map()
+        elif event.key() == Qt.Key.Key_PageDown:
+            if self.z > 1:
+                self.z -= 1
+                self.load_map()
+
 
 app = QApplication(sys.argv)
-w = QWidget()
-w.setGeometry(100, 100, 600, 450)
-w.setWindowTitle("Карта")
-
-pixmap = QPixmap("map.png")
-label = QLabel(w)
-label.move(0, 0)
-label.resize(600, 450)
-label.setPixmap(pixmap)
-
+w = MapWidget()
 w.show()
 sys.exit(app.exec())
